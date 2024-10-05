3905c056d3961374ee5d9059d8363e779443f3b1
ddolboghi <rhdfyd128@gmail.com>
2024-10-05 16:31:53 +0900

    refactor: 로그인 여부에따른 리다이렉팅 설정

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,27 @@
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 import { updateSession } from "@/utils/supabase/middleware";
+import { createClient } from "./utils/supabase/server";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+  const response = await updateSession(request);
+  const { pathname } = request.nextUrl;
+
+  const {
+    data: { user },
+  } = await createClient().auth.getUser();
+  // console.log("유저 정보 middleware: ", user);
+
+  const protectedRoutes = ["/main", "/cart", "/my"];
+
+  const isLoggedIn = user !== null;
+
+  if (protectedRoutes.includes(pathname)) {
+    if (!isLoggedIn) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
