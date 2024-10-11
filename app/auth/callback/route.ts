aa793d28ba63494c991934e8f22b510a1e570819
ddolboghi <rhdfyd128@gmail.com>
2024-10-12 00:38:58 +0900

    refactor: redirect 조건 수정

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,8 +6,6 @@ export async function GET(request: Request) {
   // by the SSR package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/server-side/nextjs
   const { searchParams, origin } = new URL(request.url);
-  console.log(origin);
-  console.log(searchParams);
   const code = searchParams.get("code");
   const next = searchParams.get("next") ?? "/";
 
@@ -17,14 +15,14 @@ export async function GET(request: Request) {
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
-      if (isLocalEnv) {
-        // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
-        return NextResponse.redirect(`${origin}${next}`);
-      } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
-      } else {
-        return NextResponse.redirect(`${origin}${next}`);
-      }
+      return NextResponse.redirect(`${origin}${next}`);
+
+      // if (forwardedHost) {
+      //   // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
+      //   return NextResponse.redirect(`https://${forwardedHost}${next}`);
+      // } else {
+      //   return NextResponse.redirect(`${origin}${next}`);
+      // }
     }
   }
 
