ac4c98e530c188a38a3f9f08509f38e0d686f75f
ddolboghi <rhdfyd128@gmail.com>
2024-10-12 10:10:16 +0900

    fix: 링크 수정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -18,7 +18,7 @@ export default async function Login() {
       <div className="flex flex-col gap-2">
         <KakaoLoginButton />
       </div>
-      <Link href="/main">메인페이지로 이동</Link>
+      <Link href="/error">에러페이지로 이동</Link>
     </main>
   );
 }
