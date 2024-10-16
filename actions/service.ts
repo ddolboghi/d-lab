9b276843add333026f4e41fcbe24257d2c846dde
ddolboghi <rhdfyd128@gmail.com>
2024-10-16 09:25:08 +0900

    feat: 서비스 CRUD 추가

--- actions/service.ts
+++ actions/service.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { supabaseClient } from "@/lib/getSupabaseClient";
-import { ServiceWithId } from "@/utils/types";
+import { ServiceWithCreatedAt, ServiceWithId } from "@/utils/types";
 import { revalidateTag } from "next/cache";
 
 export const insertService = async (addServiceFormData: FormData) => {
@@ -31,7 +31,9 @@ export const selectAllService = async () => {
           "Content-Type": "application/json",
           apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
         },
-        next: { tags: ["insertService"] },
+        next: {
+          tags: ["insertService", "updateServiceById", "deleteServiceById"],
+        },
       }
     );
 
@@ -42,5 +44,73 @@ export const selectAllService = async () => {
   } catch (e) {
     console.error("[selectAllService] Error: ", e);
     return null;
+  }
+};
+
+export const selectServiceById = async (serviceId: string) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/service?select=id,name,created_at&id=eq.${serviceId}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          apikey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
+        },
+        next: { tags: ["updateServiceById"] },
+      }
+    );
+
+    if (!response.ok) throw response.status;
+
+    const data: ServiceWithCreatedAt[] = await response.json();
+
+    if (data.length > 1) throw new Error("More than one service row exists.");
+
+    return data[0];
+  } catch (e) {
+    console.error("[selectServiceById] Error: ", e);
+    return null;
+  }
+};
+
+export const updateServiceById = async (
+  serviceId: number,
+  formData: FormData
+) => {
+  try {
+    const rawFormData = {
+      name: formData.get("name"),
+    };
+
+    const { error } = await supabaseClient
+      .from("service")
+      .update(rawFormData)
+      .eq("id", serviceId);
+
+    if (error) throw error;
+
+    revalidateTag("updateServiceById");
+    return true;
+  } catch (e) {
+    console.error("[updateServiceById]", e);
+    return false;
+  }
+};
+
+export const deleteServiceById = async (serviceId: number) => {
+  try {
+    const { error } = await supabaseClient
+      .from("service")
+      .delete()
+      .eq("id", serviceId);
+
+    if (error) throw error;
+
+    revalidateTag("deleteServiceById");
+    return true;
+  } catch (e) {
+    console.error("[deleteServiceById]", e);
+    return false;
   }
 };
