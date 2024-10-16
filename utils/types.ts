7bfaef234e756c8e0980468b35ccae8809054fd7
ddolboghi <rhdfyd128@gmail.com>
2024-10-16 09:26:02 +0900

    feat: 타입 추가

--- utils/types.ts
+++ utils/types.ts
@@ -1,5 +1,6 @@
 export interface Metadata {
-  name: string;
+  columnName: string;
+  description: string;
   type: string;
   example: string;
 }
@@ -10,4 +11,43 @@ export interface Service {
 
 export interface ServiceWithId extends Service {
   id: number;
+}
+
+export interface ServiceWithCreatedAt extends ServiceWithId {
+  created_at: Date;
+}
+
+export interface DataInfo {
+  id: number;
+  title: string;
+  url: string;
+  apikey: string;
+  metadata: Metadata[];
+  created_at: Date;
+}
+
+export interface Experiment {
+  id: number;
+  service_id: number;
+  title: string;
+  overview: string;
+  end_time: Date;
+  experimental_data_id: number;
+  experimental_data_preprocessing_id: number;
+  control_data_id: number;
+  control_data_preprocessing_id: number;
+  goal: number;
+}
+
+export interface ExperimentForRead extends Experiment {
+  created_at: Date;
+  conclusion: string;
+}
+
+export interface ExperimentForUpdate {
+  id: number;
+  title: string;
+  overview: string;
+  goal: number;
+  conclusion: string;
 }
