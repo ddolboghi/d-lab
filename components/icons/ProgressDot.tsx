a79b54841478b79a022d335c9264de20a585cea3
ddolboghi <rhdfyd128@gmail.com>
2024-10-27 17:35:00 +0900

    feat: 아이콘 추가

---
+++ components/icons/ProgressDot.tsx
@@ -0,0 +1,19 @@
+export default function ProgressDot() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="19"
+      height="19"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#B6B6B6"
+      strokeWidth="1.75"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-circle-dot"
+    >
+      <circle cx="12" cy="12" r="10" />
+      <circle cx="12" cy="12" r="1" />
+    </svg>
+  );
+}
