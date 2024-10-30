eafcc9ff2cb6ebbdac605c340136f5985a106c26
ddolboghi <rhdfyd128@gmail.com>
2024-10-30 09:47:31 +0900

    fix: cors 설정 제거

--- next.config.mjs
+++ next.config.mjs
@@ -16,31 +16,6 @@ const nextConfig = {
       },
     ];
   },
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: "https://www.d-lab.store",
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "Content-Type, Authorization",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
