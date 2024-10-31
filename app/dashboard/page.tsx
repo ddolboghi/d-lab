391f92cda51dd64b9064ca4347112993557f3ee9
ddolboghi <rhdfyd128@gmail.com>
2024-10-31 20:26:38 +0900

    style: 메인페이지 위치, 높이 수정

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -8,15 +8,15 @@ export default async function DashboardPage() {
   const services = await selectAllService();
 
   return (
-    <div className="rounded-bl-[30px] bg-[#FFF]">
-      <h1 className="font-bold text-[22px] pl-8 pt-[20px]">Projects</h1>
+    <div className="translate-y-[50px] rounded-bl-[30px] bg-[#FFF] pb-8 pl-8 overflow-y-auto h-screen">
+      <h1 className="font-bold text-[22px] pt-[20px]">Projects</h1>
       <AddService />
-      <ul className="list-none grid grid-rows-1 grid-flow-col auto-cols-max gap-[50px] pl-8 pb-8 w-full">
+      <ul className="list-none grid grid-rows-1 grid-flow-col auto-cols-max gap-[50px] w-full overflow-x-auto">
         {services ? (
           services.map((service) => (
             <li
               key={service.id}
-              className="relative rounded-[25px] bg-[#F0F2F5] w-[300px] text-center px-4  min-h-[100px]"
+              className="relative rounded-[25px] bg-[#F0F2F5] w-[300px] text-center px-4 min-h-[100px]"
             >
               <ServiceInfo service={service} />
               <ExperimentRegister serviceId={service.id} />
